b604ff34120d7548408ac139e2e8fc28cefeab86
Marcos Baravalle <marcosabaravalle@gmail.com>
2022-11-05 12:42:21 -0300

    content adding & bug fixes

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const openBtn = document.querySelector(".hamburger-button");
 const closeBtn = document.querySelector(".close-button");
 const navBar = document.querySelector("nav");
 
-function replaceEmotes() {
+// LINKS SELECTION
+const linkSelect = () => {
   const links = document.querySelectorAll(".link");
 
   links.forEach((link) => {
@@ -13,8 +14,8 @@ function replaceEmotes() {
       navBar.classList.toggle("active");
     });
   });
-}
-replaceEmotes();
+};
+linkSelect();
 
 openBtn.addEventListener("click", () => {
   navBar.classList.toggle("active");
@@ -24,20 +25,23 @@ closeBtn.addEventListener("click", () => {
   navBar.classList.toggle("active");
 });
 
-// SLIDER LOGIC
-let counter = 1;
+// CAROUSEL LOGIC
 
-document.querySelector(".next").addEventListener("click", () => {
-  document.getElementById("radio" + counter).checked = true;
-  counter++;
-  if (counter > 2) {
-    counter = 1;
-  }
-});
-document.querySelector(".prev").addEventListener("click", () => {
-  document.getElementById("radio" + counter).checked = true;
-  counter--;
-  if (counter < 1) {
-    counter = 2;
-  }
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
 });
